5d029e730f00cd4a4313ec0bd266d92270bbdcd1
Yvan Pangilinan <yvanp@Yvans-MacBook-Pro.local>
2018-03-27 09:22:22 -0400

    added login auth w/ firebase, added installation instruction on README, added spinner on loading and error catchon login

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from 'firebase';
 import Router from './Router';
+import FirebaseConfig from '../config/FirebaseConfig';
 
-class App extends React.Component {
+class App extends Component {
+
+  componentWillMount() {
+      firebase.initializeApp(FirebaseConfig);
+  }
+
   render() {
     return (
       <Router />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
+import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { Button, Input, CardSection } from './common';
+import { Button, Input, CardSection, Spinner } from './common';
 import Logo from '../img/knapsack_logo.png';
 
 
 class Login extends Component {
   state = {
-     userName: '',
-     password: ''
+     email: '',
+     password: '',
+     error: '',
+     loading: false
+  }
+
+  onLogginSuccess() {
+    this.setState({ email: '', password: '', loading: false, error: '' });
+    Actions.main();
+  }
+
+  onLogginFail() {
+    this.setState({ error: 'Authentication Failed.', loading: false });
+  }
+
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+   this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLogginSuccess.bind(this))
+      .catch(this.onLogginFail.bind(this));
+  }
+
+  renderLoginButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button
+        accessibility
+        Label={'Click to log in!'}
+        onPress={this.onButtonPress.bind(this)}
+        style={{ backgroundColor: '#57d1c9' }}
+        accessible
+      >
+        <Text style={styles.buttonText}> LOGIN </Text>
+      </Button>
+    );
+  }
+
+  renderSignupButton() {
+    return (
+      <Button
+        accessibilityLabel={'Click to sign up!'}
+        onPress={() => Actions.signUp()}
+        style={{ backgroundColor: '#57d1c9' }}
+        accessible
+      >
+          <Text style={styles.buttonText}>SIGN UP</Text>
+      </Button>
+    );
   }
 
   render() {
@@ -25,7 +79,7 @@ class Login extends Component {
             style={styles.inputStyle}
             label="Email"
             placeholder="email@gmail.com"
-            onChangeText={(text) => this.setState({ userName: text })}
+            onChangeText={(email) => this.setState({ email })}
             value={this.state.userName}
           />
         </CardSection>
@@ -36,31 +90,21 @@ class Login extends Component {
             style={styles.inputStyle}
             label="Password"
             placeholder="password"
-            onChangeText={(text) => this.setState({ password: text })}
+            onChangeText={(password) => this.setState({ password })}
             value={this.state.password}
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button
-            accessibilityLabel={'Click to log in!'}
-            onPress={() => Actions.main()}
-            style={{ backgroundColor: '#57d1c9' }}
-            accessible
-          >
-            <Text style={styles.buttonText}> LOGIN </Text>
-           </Button>
+          {this.renderLoginButton()}
         </CardSection>
 
         <CardSection>
-          <Button
-            accessibilityLabel={'Click to sign up!'}
-            onPress={() => Actions.signUp()}
-            style={{ backgroundColor: '#57d1c9' }}
-            accessible
-          >
-              <Text style={styles.buttonText}>SIGN UP</Text>
-          </Button>
+          {this.renderSignupButton()}
         </CardSection>
 
       </View>
@@ -89,6 +133,11 @@ const styles = {
     height: 200,
     width: 200,
     borderColor: '#ddd',
+  },
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 };
 
