9a326fdaba2a9f6d4df282edb5ad3777e09c290e
Christopher Menedes <cmenedes@yahoo.com>
2018-04-24 01:18:35 -0400

    added tabbar and nested scenes for navigation

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
+import { StyleSheet } from 'react-native';
 import Login from './components/Login';
 import RecommendList from './components/RecommendList';
 import SignUp from './components/SignUp';
+import { TabIcon } from './components/common/TabIcon.js';
 
 
 const RouterComponent = () => {
@@ -13,12 +15,58 @@ const RouterComponent = () => {
           <Scene key="login" component={Login} initial />
           <Scene key="signUp" component={SignUp} />
         </Scene>
-        <Scene key="main">
-          <Scene key="recommendList" title="Recommended List" component={RecommendList} />
+        <Scene key="main" hideNavBar>
+          <Scene
+            key="tabbar"
+            tabs
+            lazy
+            tabBarStyle={styles.navBar}
+            tabBarPosition="bottom"
+            showLabel={false}
+            inactiveBackgroundColor={'lightgray'}
+            activeBackgroundColor={'#25abf9'}
+          >
+            <Scene key="Knapsack" title="Knapsack" icon={TabIcon}>
+              <Scene
+                key="Knapsack"
+                title="Knapsack"
+                component={RecommendList}
+                initial
+              />
+              {/* TODO: Create Knapsack component and replace above */}
+            </Scene>
+            <Scene key="Recommended" title="Recommended List" icon={TabIcon}>
+              <Scene
+                key="RecommendList"
+                title="Recommended List"
+                component={RecommendList}
+                initial
+              />
+              { /* TODO: Scene for video player content goes here */ }
+            </Scene>
+            <Scene key="Profile" title="Profile" icon={TabIcon}>
+              <Scene
+                key="Profile"
+                title="Profile"
+                component={RecommendList}
+                initial
+              />
+            {/* TODO: Create Profile component and replace above */}
+            </Scene>
+          </Scene>
         </Scene>
       </Scene>
     </Router>
   );
 };
+
+const styles = StyleSheet.create({
+  navBar: {
+    borderTopWidth: 0.5,
+    borderColor: '#b7b7b7',
+    opacity: 1
+    },
+  }
+);
 
 export default RouterComponent;
