0e77ac4871fb072a30353c2618c48745e528b083
Yvan Pangilinan <yvanp@Yvans-MacBook-Pro.local>
2018-05-14 11:45:03 -0400

    applied bug fix rm package.json + rerun packager

--- android/app/src/main/java/com/knapsack/MainApplication.java
+++ android/app/src/main/java/com/knapsack/MainApplication.java
@@ -6,6 +6,12 @@ import com.facebook.react.ReactApplication;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
+import com.brentvatne.react.ReactVideoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,6 +32,12 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeYouTube(),
+            new ReactVideoPackage(),
+            new VectorIconsPackage(),
+            new ReactNativeYouTube(),
+            new ReactVideoPackage(),
+            new VectorIconsPackage(),
             new ReactNativeYouTube(),
             new ReactVideoPackage(),
             new VectorIconsPackage()
